d441a2faea4642a590e884232a521707f7ca692e
Jacob Hensley <jacobrhensley3@gmail.com>
2020-05-04 12:04:18 -0500

    do-while

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,11 +1,34 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
+//        int i = 5;
+//
+//        while(i <= 15){
+//            System.out.println("i is " + i);
+//            i++;
+//        }
 
-        while(i <= 15){
-            System.out.println("i is " + i);
-            i++;
-        }
+//        int i = 0;
+//        do {
+//            System.out.println("i is " + i);
+//            i++;
+//        } while (i <= 100);
+
+//        int i = 100;
+//        do {
+//            System.out.println("i is " + i);
+//            i -= 5;
+//        } while (i >= -10);
+
+//        long i = 2;
+//        do {
+//            System.out.println("i is " + i);
+//            i = i * i;
+//        } while (i <= 1000000);
+
+
+
+
+
+
     }
-
 }
