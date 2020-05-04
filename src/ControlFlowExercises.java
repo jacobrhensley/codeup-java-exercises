df451021d0f9c5bfef808b830f6cf4d1c8bcddea
Jacob Hensley <jacobrhensley3@gmail.com>
2020-05-04 12:20:48 -0500

    fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -34,6 +34,16 @@ public class ControlFlowExercises {
 //            System.out.println("i is " + i);
 //        }
 
+        for(byte i = 0; i <= 100; i++){
+            boolean check = true;
+            if(i % 5 == 0 && i % 3 == 0){
+                System.out.println("FizzBuzz");
+            }else if(i % 3 == 0){
+                System.out.println("Fizz");
+            } else if(i % 5 == 0){
+                System.out.println("Buzz");
+            }
+        }
 
     }
 }
